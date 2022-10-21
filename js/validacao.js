08c633df81cae847ac2dee525a4526ae00db0e56
HMenezes <humberto.menezes47@hotmail.com>
2022-10-21 12:01:09 -0300

    new_featureV4

--- js/validacao.js
+++ js/validacao.js
@@ -40,12 +40,30 @@ const mensagensDeErro = {
     cpf: {
         valueMissing: 'O campo de CPF não pode estar vazio.',
         customError: 'O CPF digitado não é válido.' 
+    },
+    cep:{
+        valueMissing:'O campo de CEP não pode estar vazio',
+        patternMismatch:'O CEP digitado não é válido.',
+        customError:'CEP não encontrado'
+    },
+    logradouro:{
+        valueMissing:'O campo de logradouro não pode estar vazio.'        
+    },
+    cidade:{
+        valueMissing:'O campo de cidade não pode estar vazio.'     
+    },
+    estado:{
+        valueMissing:'O campo de estado não pode estar vazio.'
+    },
+    preco:{
+        valueMissing:'O campo de preço não pode estar vazio.'
     }
 }
 
 const validadores = {
     dataNascimento:input => validaDataNascimento(input),
-    cpf:input => validaCPF(input)
+    cpf:input => validaCPF(input),
+    cep:input => recuperarCEP(input)
 }
 
 function mostraMensagemDeErro(tipoDeInput, input) {
@@ -144,8 +162,39 @@ function confirmaDigito(soma) {
 }
 
 
-// 123 456 789 09
+function recuperarCEP(input){
+    const cep = input.value.replace(/\D/g,'')
+    const url = `https://viacep.com.br/ws/${cep}/json/`
+    const options = {
+        method: 'GET', 
+        mode:'cors',
+        headers:{
+            'content-type': 'application/json;charse=utf-8'
+        }
+    }
+    if(!input.validy.patternMismatch && !input.validity.valueMissing){
+        fetch(url,options).then(
+            response => response.json()
+        ).then(
+            data =>{
+                if(data.erro){
+                    input.setCustomValidity('CEP não encontrado')
+                    return
+                }
+                input.setCustomValidity('')
+                preencheCamposComCEP(data)
+                return
+            }
+        )
+    }
+}
 
-// let soma = (11 * 1) + (10 * 2) + (9 * 3) ... (2 * 0)
+function preencheCamposComCEP(data){
+    const logradouro = document.querySelector('[data-tipo="logradouro"]')
+    const cidade = document.querySelector('[data-tipo="cidade"]')
+    const estado = document.querySelector('[data-tipo="estado"]')
 
-// let digitoVerificador = 11 - (soma % 11)
+    logradouro.value = data.logradouro
+    cidade.value = data.localidade
+    estado.value = data.uf
+}
